78cc805ad154b550c6b128b1d71fdcfcaf31ba50
朗吾東 <113815219+langwudong@users.noreply.github.com>
2023-12-31 20:40:26 +0800

    Update openai.go

--- openai/openai.go
+++ openai/openai.go
@@ -1,14 +1,53 @@
 package openai
 
-const (
-	// 聊天API
-	OPENAI_CHAT_API = "https://one.aiskt.com/v1/chat/completions"
-	// 画图API
-	OPENAI_DARW_API = "https://api.openai.com/v1/images/generations"
-	// 情感分析API
-	OPENAI_EMATION_API = "https://api.openai.com/v1/moderations"
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
 )
 
 type OpenAI struct {
 	Organization string
+}
+
+func (openai *OpenAI) CallCompletions(completionsAPI string, requestBody RequestBody) string {
+	data, err := json.Marshal(requestBody)
+	if err != nil {
+		return err.Error()
+	}
+
+	req, err := http.NewRequest("POST", completionsAPI, bytes.NewBuffer(data))
+	if err != nil {
+		log.Fatal(err)
+		return err.Error()
+	}
+
+	req.Header.Set("Content-Type", "application/data")
+	req.Header.Set("Authorization", "Bearer "+openai.Organization)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+		return err.Error()
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+		return err.Error()
+	}
+
+	var response ResponseBody
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		log.Fatal(err)
+		return err.Error()
+	}
+
+	return response.Choices[0].Message.Content
 }
