09fd608687223011f23ac7ba474d25bae0918b2a
朗吾東 <113815219+langwudong@users.noreply.github.com>
2023-12-31 23:58:29 +0800

    Update common.go

--- openai/common.go
+++ openai/common.go
@@ -2,7 +2,7 @@ package openai
 
 const (
 	// 聊天API
-	OPENAI_CHAT_API = "https://one.aiskt.com/v1/chat/completions"
+	OPENAI_CHAT_API = "https://api.openai.com/v1/chat/completions"
 	// 画图API
 	OPENAI_DARW_API = "https://api.openai.com/v1/images/generations"
 	// 情感分析API
